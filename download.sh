cc484fe69a164aa894bb1d04e7decb71ad292f39
Ghadeer Mobasher <53903145+mobashgr@users.noreply.github.com>
2023-01-11 09:12:31 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,11 +4,22 @@
 #reterived from BioBERT (https://github.com/dmis-lab/biobert) via this link (https://drive.google.com/file/d/1cGqvAm9IZ_86C4Mj7Zf-w9CFilYVDl8j/view) and
 #BioRED dataset is publically avaliable in https://ftp.ncbi.nlm.nih.gov/pub/lu/BioRED/. The datasets are used for fine-tuing from scratch
 
-gdown https://drive.google.com/uc?id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0
-unzip datasets.zip
-rm -r datasets.zip
+set -e
 
+# Configure download location
+DOWNLOAD_PATH="$BIOBERT_DATA"
+if [ "$BIOBERT_DATA" == "" ]; then
+    echo "BIOBERT_DATA not set; downloading to default path ('data')."
+    DOWNLOAD_PATH="./data"
+fi
+DOWNLOAD_PATH_TAR="$DOWNLOAD_PATH.zip"
+
+# Download datasets
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0" -O "$DOWNLOAD_PATH_TAR" && rm -rf /tmp/cookies.txt
+unzip "$DOWNLOAD_PATH_TAR"
+rm "$DOWNLOAD_PATH_TAR"
 echo "Bio dataset download done!"
+
 
 
 # BC7T2-evaluation_v3 folder is directly 
